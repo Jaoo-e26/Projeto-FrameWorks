a6337219e865c97ae49c644e0882b87e2c3862fb
Jaoo-e26 <j.emanuel0929@gmail.com>
2026-09-21 19:07:17 -0300

    feat: adiciona rotas de findUnique, update e delete de alunos

--- src/routes/alunoRouter.js
+++ src/routes/alunoRouter.js
@@ -1,10 +1,19 @@
-const express = require("express");
-const alunoController = require("../controllers/AlunoController");
+const { Router } = require('express');
+const alunoController = require('../controllers/AlunoController');
 
-const router = express.Router();
+const router = Router();
 
-router.get("/", alunoController.findMany);
-router.post("/", alunoController.create);
-router.delete("/:id", alunoController.delete);
+// Requisito 1: Listagem com paginação, ordenação (orderBy/order) e total de registros
+router.get('/alunos', (req, res) => alunoController.findMany(req, res));
+
+// Requisito 2: Buscar um único aluno pelo ID
+router.get('/alunos/:id', (req, res) => alunoController.findUnique(req, res));
+
+// Requisito 3: Atualizar um aluno por ID (suporta PUT e PATCH)
+router.put('/alunos/:id', (req, res) => alunoController.update(req, res));
+router.patch('/alunos/:id', (req, res) => alunoController.update(req, res));
+
+// Requisito 4: Remover um aluno por ID
+router.delete('/alunos/:id', (req, res) => alunoController.delete(req, res));
 
 module.exports = router;
